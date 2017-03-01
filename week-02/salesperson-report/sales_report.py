d440ec62c622c894fd32f7fa7c2ab04ea92f6fbb
Katie <knt.taylor@gmail.com>
2017-03-01 03:39:01 +0000

    changed lists into a single dictionary and added salesperson and their total melons to the dictionary.Also sorted alphabetically before printing

--- week-02/salesperson-report/sales_report.py
+++ week-02/salesperson-report/sales_report.py
@@ -3,23 +3,23 @@ sales_report.py - Generates sales report showing the total number
                   of melons each sales person sold.
 """
 
-salespeople = []
-melons_sold = []
+salespeople_totals = {}
 
-f = open("sales-report.txt")
-for line in f:
+# open file
+text = open("sales-report.txt")
+
+# iterate of each line and split in a list
+for line in text:
     line = line.rstrip()
     entries = line.split("|")
+
+    # create a variable from the list for salesperson and melons sold
     salesperson = entries[0]
     melons = int(entries[2])
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-        melons_sold[position] += melons
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+    salespeople_totals[salesperson] = salespeople_totals.get(salesperson, 0) + melons
 
+salespeople = sorted(salespeople_totals.keys())
 
-for i in range(len(salespeople)):
-    print "{} sold {} melons".format(salespeople[i], melons_sold[i])
+for salesperson in salespeople:
+    print "{} sold {} melons".format(salesperson, salespeople_totals[salesperson])
